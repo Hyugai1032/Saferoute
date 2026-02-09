e445fabb3b3c82717769b311ebcfcd523cd754bb
Hyugai1032 <rosaleskaentkraven@gmail.com>
2026-02-09 22:47:51 +0800

    Added .env.example

--- capstone-frontend/src/views/Staff/StaffDashboard.vue
+++ capstone-frontend/src/views/Staff/StaffDashboard.vue
@@ -1,32 +1,29 @@
 <template>
   <div style="padding: 20px;">
     <div class="header-actions">
-          <button class="logout-btn" @click="logout">
-            <i class="icon-logout"></i>
-            Logout
-          </button>
-        </div>
+      <button class="logout-btn" @click="logout">
+        <i class="icon-logout"></i>
+        Logout
+      </button>
+    </div>
+
     <h1>Staff Dashboard</h1>
     <p>Quick access for evacuation staff (centers + logs).</p>
   </div>
 </template>
 
 <script setup>
-
-import { useRouter } from 'vue-router'
-
-</script>
-<script>
-export default { name: "StaffDashboard" };
+import { useRouter } from "vue-router";
 
 const router = useRouter();
 
 const logout = () => {
-  localStorage.removeItem('access_token');
-  localStorage.removeItem('refresh_token');
-  localStorage.removeItem('isAuthenticated');
-  localStorage.removeItem('userData');
-  router.push('/auth/login');
-}
+  localStorage.removeItem("access_token");
+  localStorage.removeItem("refresh_token");
+  localStorage.removeItem("isAuthenticated");
+  localStorage.removeItem("userData");
 
+  router.push("/auth/login");
+  // or: router.replace("/auth/login"); // prevents going back
+};
 </script>
